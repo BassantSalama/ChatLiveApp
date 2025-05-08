d2da158c2bbabec91fc9d097f2fc018b5d3e4e29
Basant <basantsalama05@gmail.com>
2025-05-08 11:44:46 +0300

    Added UIScrollView to wrap all screen content and Embedded all UI elements inside a ContentView

--- ChatLiveApp/Presentation/Modules/Login/Views/ViewController/LoginViewController.swift
+++ ChatLiveApp/Presentation/Modules/Login/Views/ViewController/LoginViewController.swift
@@ -1,26 +1,18 @@
 
-
 import UIKit
 
-class LoginVC: UIViewController {
-    
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var forgetPasswordLbl: UILabel!
+    @IBOutlet weak var forgetPasswordLabel: UILabel!
     @IBOutlet weak var signInButton: UIButton!
-    @IBOutlet weak var newUserLbl: UILabel!
+    @IBOutlet weak var newUserLabel: UILabel!
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-
-        
     }
-    
-
-    
-
 }
